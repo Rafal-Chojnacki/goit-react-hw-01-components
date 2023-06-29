33cd9e28db5ba82a01f30d26cc801f9266bd6c00
Rafał Chojnacki <r.chojnacki2@o2.pl>
2023-06-29 12:02:09 +0200

    Added History of transaction

--- src/components/jsx/transactionHistory.jsx
+++ src/components/jsx/transactionHistory.jsx
@@ -5,20 +5,26 @@ const dataArray = transactions.map(transaction => ({
     amount: transaction.amount,
     currency: transaction.currency
   }));
-  console.log(dataArray);
 
   export const TransactionsHistory = (props) => {
     return (
-        <section>
-    <ul>
-      {dataArray.map(item => (
-        <li className="item" key={item.id}>
-          <span className="type">{item.type}</span>
-          <span className="amount">{item.amount}</span>
-          <span className="currency">{item.currency}</span>
-        </li>
-      ))}
-    </ul>
-    </section>
+      <table class="transaction-history">
+  <thead>
+    <tr className='theadRow'>
+      <th >Type</th>
+      <th>Amount</th>
+      <th>Currency</th>
+    </tr>
+  </thead>
+  <tbody>
+  {dataArray.map(item => (
+        <tr className="transaction-item" key={item.id}>
+      <td className='typeOfTransaction'>{item.type}</td>
+      <td>{item.amount}</td>
+      <td>{item.currency}</td>
+    </tr>
+    ))}
+  </tbody>
+</table>
     )
   }
