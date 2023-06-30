64546c7617b472cd18eeb9fcacb5fcb075c8a191
Rafał Chojnacki <r.chojnacki2@o2.pl>
2023-06-30 13:53:54 +0200

    Added random color for ststistics list

--- src/components/statistics/statistics.jsx
+++ src/components/statistics/statistics.jsx
@@ -3,14 +3,18 @@ import data from './data';
 import css from "./statistics.module.css";
 
   const dataArray = data
-
+let colors = ['#7CAEEE', '#CF7CEE', '#E91C51', '#4b9fff'];
 export const Statistics = () => {
   return (
+    
     <section className={css.statistics}>
     <h2 className={css.title}>Upload stats</h2>
     <ul className={css.list}>
       {dataArray.map(item => (
-        <li className={css.item} key={item.id}>
+        <li className={css.item} key={item.id}
+        style={{
+          backgroundColor: colors[Math.floor(Math.random() * colors.length)]
+        }}>
           <span className={css.label}>{item.label}</span>
           <span className={css.percentage}>{item.percentage}%</span>
         </li>
@@ -19,6 +23,7 @@ export const Statistics = () => {
     </section>
   )
   }
+
 
   Statistics.propTypes = {
     id: PropTypes.number,
